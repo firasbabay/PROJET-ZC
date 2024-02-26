3d843d6aca0616a275460a91f62008b060258095
safouen ourabi <141176964+safwenourabi@users.noreply.github.com>
2024-02-26 21:46:45 +0100

    Update conn.js

--- Server/db/conn.js
+++ Server/db/conn.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE;
 
-mongoose.connect(DB).then(() => {
+const uri = 'mongodb://mongo:FA5hFB1deD62CcG6H3DD1fBhcAEA1C62@viaduct.proxy.rlwy.net:47990';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
     console.log('connection successful');
-}).catch((err) => console.log('Connection Unsuccessful :('));
+}).
+catch((err) => console.log('Connection Unsuccessful :('));
